0aa11232782984ae3a5a10ef10bac0ce76c387ea
Yashowardhan <yashowardhanshinde@gmail.com>
2023-11-30 00:41:55 -0800

    scatter plot added

---
+++ scripts/scatter.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+def scatter_plot(df: pd.DataFrame, x_col: str, y_col: str, xaxis_title: str, yaxis_title: str, title: str):
+    '''
+    Function that plots a scatter plots between 2 features of a dataset.
+    
+    Parameters:
+        df  (pd.DataFrame): Pandas Dataframe
+        x_col   (str): Feature of dataset to be plotted on x axis
+        y_col   (str): Feature of dataset to be plotted on y axis
+        xaxis_title (str): X axis label
+        yaxis_title (str): Y axis label
+        title   (str): Title of Plot
+    Returns:
+        None
+    '''
+    
+    assert isinstance(df, pd.DataFrame) and isinstance(x_col, str) and isinstance(title, str)
+    assert isinstance(y_col, str) and isinstance(xaxis_title, str) and isinstance(yaxis_title, str)
+    assert x_col in list(df.columns) and y_col in list(df.columns)
+    
+    fig = px.scatter(df, x=x_col, y=y_col, trendline='ols', hover_data=['City'])
+    fig.update_layout(title=title,
+                    xaxis_title=xaxis_title,
+                    yaxis_title=yaxis_title)
+    fig.show()
